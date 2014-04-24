f08fb2c8d04dfde71dfb9e6de2d610abf237d82c
Sean Dunn <sedunn@adobe.com>
2014-04-24 10:50:59 -0400

    Packages: Remove unneeded build packages/tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,24 +8,14 @@
 
 'use strict';
 
-var path = require('path');
-var fs = require('fs');
-
 module.exports = function(grunt) {
-
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
     options: {
-      buildDir: 'public/assets-built',
-      tasksDir: 'tasks',
       junitFile: grunt.option('junitFile') || 'test/jasmine/report.xml',
       coverageDir: grunt.option('coverageDir') || 'test/jasmine/coverage'
-    },
-
-    clean: {
-      build: ['<%= options.buildDir %>']
     }
   });
 
