8b2dc03c2214b79d33133b89108e6a907232b1ca
Joel Kemp <jokemp@adobe.com>
2015-04-30 11:50:18 -0400

    Time Grunt: Time grunt tasks

--- tasks/aliases.js
+++ tasks/aliases.js
@@ -1,4 +1,6 @@
 module.exports = function(grunt) {
+  require('time-grunt')(grunt);
+
   grunt.registerTask('sniff', ['jscs:main']);
   grunt.registerTask('lint', ['jshint', 'jscs:main', 'y_u_no_bundle', 'build_lint']);
   grunt.registerTask('test', ['lint', 'karma:single']);
