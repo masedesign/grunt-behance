a4efe2344543e9deabe0f3109a21ed46d489afef
Joel Kemp <jokemp@adobe.com>
2015-07-08 13:21:47 -0400

    Remove the usage of y_u_no_bundle due to hard to debug singularity failures

--- tasks/aliases.js
+++ tasks/aliases.js
@@ -2,7 +2,7 @@ module.exports = function(grunt) {
   require('time-grunt')(grunt);
 
   grunt.registerTask('sniff', ['jscs:main']);
-  grunt.registerTask('lint', ['jshint', 'jscs:main', 'y_u_no_bundle', 'build_lint']);
+  grunt.registerTask('lint', ['jshint', 'jscs:main', 'build_lint']);
   grunt.registerTask('test', ['lint', 'karma:single']);
 
   grunt.registerTask('js', ['test']);
